7eb3744face3fc24ab1b81c9d49f80b0f4d64961
agu <iba.agustin@gmail.com>
2023-05-08 22:13:01 -0300

    added dynamic routes setup, using getStaticPaths and getStaticProps, and writing util functions for both. One that grabs all possible values of dynamic route, and the other one that fetches necessary data depending on the route

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
+import path from "path";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import utilStyles from "../styles/utils.module.css";
 import Layout, { siteTitle } from "../components/layout";
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+// console.log(path.join(process.cwd(), "posts"));
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -15,6 +26,20 @@ export default function Home() {
           Hi, I'm Judy. Arguably the best character in Night City. Braindance
           techie, and Mox member.
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
